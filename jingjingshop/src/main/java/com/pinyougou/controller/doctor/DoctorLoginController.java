95cec76a13e1bb303b82a8c21ce6d946357745c7
clearyue123 <clearyue123@sina.com>
2019-06-15 13:28:39 +0800

    代码提交 乐晶晶

--- jingjingshop/src/main/java/com/pinyougou/controller/doctor/DoctorLoginController.java
+++ jingjingshop/src/main/java/com/pinyougou/controller/doctor/DoctorLoginController.java
@@ -1,11 +1,14 @@
 package com.pinyougou.controller.doctor;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pinyougou.common.ApiResult;
+import com.pinyougou.mapper.TbDoctorMapper;
 import com.pinyougou.pojo.TbDoctor;
 import com.pinyougou.service.doctor.DoctorService;
 
@@ -23,29 +26,30 @@ import util.TextUtils;
 public class DoctorLoginController {
 	@Autowired
 	private DoctorService doctorService;
-
-	private IdWorker idWorker = new IdWorker();
+    @Autowired
+    private TbDoctorMapper doctorMapper;
 	
 	@RequestMapping(value = "/wxLogin", method = RequestMethod.POST)
 	public ApiResult wxlogin(String openid, String unionid) {
-		TbDoctor user = new TbDoctor();
+		TbDoctor doctor = new TbDoctor();
 		if(openid!=null&&openid.trim().length()>0){
-			user.setOpenId(openid);
+			doctor.setOpenId(openid);
 		}
 		if(unionid!=null&&unionid.trim().length()>0){
-			user.setUnionId(unionid);
+			doctor.setUnionId(unionid);
 		}
-		user.setPoints(0);
-		user.setPointsAll(0);
-		TbDoctor result = doctorService.firstInfo(user);//union查
+		doctor.setPoints(0);
+		doctor.setPointsAll(0);
+		doctor.setCreateDate(new Date());
+		TbDoctor result = doctorService.firstInfo(doctor);//union查
 		if (result != null) {
 			if(TextUtils.isBlank(result.getName())){
 				return new ApiResult(201, "登录成功，请绑定微信昵称和头像", result);
 			}
 			return new ApiResult(200, "登录成功", result);
 		} else {
-			doctorService.add(user);
-			TbDoctor result1 = doctorService.firstInfo(user);
+			doctorMapper.insert(doctor);
+			TbDoctor result1 = doctorService.firstInfo(doctor);
 			return new ApiResult(201, "登录成功，请绑定微信昵称和头像", result1);
 		}
 	}
@@ -82,10 +86,8 @@ public class DoctorLoginController {
 			}
 			return new ApiResult(200, "绑定成功", tbUser);
 		} else {
-			long doctorId = idWorker.nextId();
-			user.setDid(doctorId);
-			doctorService.add(user);
-			TbDoctor tbUser = doctorService.selectById(doctorId);
+			doctorMapper.insert(user);
+			TbDoctor tbUser = doctorService.firstInfo(user);
 			if (tbUser == null) {
 				return new ApiResult(101, "未知错误，请联系管理员", tbUser);
 			}
